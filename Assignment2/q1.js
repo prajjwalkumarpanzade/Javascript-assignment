54ea9a5744e43412dcb4b2d25cc4895161e515ba
prajjwalkumarpanzade <prajjwalbpanzade22@gmail.com>
2024-02-16 15:29:17 +0530

    Assignment2 Q1 Answer

---
+++ Assignment2/q1.js
@@ -0,0 +1,10 @@
+let a;
+console.log(typeof a);
+a = 15;
+console.log(typeof a);
+const b = "Prajjwalkumar";
+console.log(typeof b);
+b = 20
+console.log(typeof b);
+
+//when we try to re-assign value to constatnt it throws an error
