06069a1b64a58964601d224313a0cf9c7178ab69
prajjwalkumarpanzade <prajjwalbpanzade22@gmail.com>
2024-02-18 23:03:25 +0530

    Question 1 Answer

--- Assignment3/q1.js
+++ Assignment3/q1.js
@@ -1,8 +1,9 @@
 let a = 25;
-if(a <= 12){
+
+if (a > 0 && a <= 12){
 	console.log("Child");
 } else if(a >= 13 && a <= 18){
 	console.log("Teen");
 } else{
-	console.log("Adult")
+	console.log("Adult");
 }
