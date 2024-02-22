02bfa664aa64dd2658167efa77161e2877f0e07f
prajjwalkumarpanzade <prajjwalbpanzade22@gmail.com>
2024-02-22 12:48:50 +0530

    Js Fundamentals Object Assignment Question 1 Answer

---
+++ Assignment7/q3.js
@@ -0,0 +1,23 @@
+let users = [{
+  "id": 1,
+  "first_name": "Nicki",
+  "email": "ncrozier0@squarespace.com",
+  "date_of_birth": "2009/05/09"
+  }, {
+  "id": 2,
+  "first_name": "Raychel",
+  "email": "rmcgrady1@cpanel.net",
+  "date_of_birth": "1996/11/05"
+  }];
+
+function mapBy(users,value) {
+  let newobj = {};
+  for (const i in users) {
+    newobj[users[i][value]] = users[i];
+    // console.log(users[i][value]);
+}
+console.log(newobj);
+};
+
+value = 'email'
+mapBy(users,value);
