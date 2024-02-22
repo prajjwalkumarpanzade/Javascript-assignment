0a42a7954ff2efe1e24f453496411fa3205e14d2
prajjwalkumarpanzade <prajjwalbpanzade22@gmail.com>
2024-02-22 13:07:21 +0530

    Js Fundamentals Object Question 2 Answer

---
+++ Assignment7/q4.js
@@ -0,0 +1,39 @@
+let users = [{
+  "id": 1,
+  "first_name": "Nicki",
+  "email": "ncrozier0@squarespace.com",
+  "date_of_birth": "2009/05/09",
+  "gender":"Male",
+  }, {
+  "id": 2,
+  "first_name": "Raychel",
+  "email": "rmcgrady1@cpanel.net",
+  "date_of_birth": "1996/11/05",
+    "gender":"Female"
+  }, {
+  "id": 3,
+  "first_name": "Demetris",
+  "email": "dkilshall2@elpais.com",
+  "date_of_birth": "2018/12/31",
+    "gender":"Male"
+  }, {
+  "id": 4,
+  "first_name": "Amata",
+  "email": "abraiden3@canalblog.com",
+  "date_of_birth": "2012/05/23",
+    "gender":"Female"
+  }];
+
+  function groupBy(array, key) {
+    return array.reduce((result, currentValue) => {
+      const groupKey = currentValue[key];
+      if (!result[groupKey]) {
+        result[groupKey] = [];
+      }
+      // Push the current object to the array corresponding to the key
+      result[groupKey].push(currentValue);
+      return result;
+    }, {});
+  }
+
+  console.log(groupBy(users,'email'));
