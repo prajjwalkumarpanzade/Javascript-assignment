7224e42addf695257b5a637777caddcc80400a1f
prajjwalkumarpanzade <prajjwalbpanzade22@gmail.com>
2024-02-26 14:58:27 +0530

    Add Output as comment

--- Assignment2/q2.js
+++ Assignment2/q2.js
@@ -33,6 +33,63 @@ obj1["email"] = "prajjwal@gmail.com";
 console.log("Updated object - ",obj2);
 delete obj1["department"];
 console.log("After deleting department - ", obj2);
+`
+OUTPUT
+Original Array -  [
+  5, 2, 3, 6,
+  9, 8, 7, 1
+]
+Type of a =  object
+Array after adding 13 and 55 -  [
+  5, 2, 3,  6,  9,
+  8, 7, 1, 13, 55
+]
+Array after deleting last element -  [
+  5, 2, 3,  6, 9,
+  8, 7, 1, 13
+]
+Sorted array -  [
+  1, 13, 2, 3, 5,
+  6,  7, 8, 9
+]
+Original array -  [ 50, 65, 48, 21, 15, 35 ]
+Type of b =  object
+Array after adding 0 -  [
+  50, 65, 48, 21,
+  15, 35,  0
+]
+Array after removing last element -  [ 50, 65, 48, 21, 15, 35 ]
+Sorted array -  [ 15, 21, 35, 48, 50, 65 ]
+{ name: 'Prajjwalkumar', age: 23, add: 'Model colony Pune' }
+Type of obj1 -  object
+Updated object -  {
+  name: 'Prajjwalkumar',
+  age: 23,
+  add: 'Model colony Pune',
+  email: 'prajjwal@gmail.com'
+}
+After deleting age -  {
+  name: 'Prajjwalkumar',
+  add: 'Model colony Pune',
+  email: 'prajjwal@gmail.com'
+}
+{
+  college: 'Fergusson college',
+  department: 'Computer Science',
+  add: 'FC Road Pune'
+}
+Type of obj2 -  object
+Updated object -  {
+  college: 'Fergusson college',
+  department: 'Computer Science',
+  add: 'FC Road Pune'
+}
+After deleting department -  {
+  college: 'Fergusson college',
+  department: 'Computer Science',
+  add: 'FC Road Pune'
+}
+`
 
 
 
